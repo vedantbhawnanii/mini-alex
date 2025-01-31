7fd8bac5d1cfdc7bbbc0ed12d4cc31ee644a294f
vedantbhawnani <vedant.bhawnani@gmail.com>
2025-01-31 22:58:54 +0530

    streamlit fixes #6

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import streamlit as st
-from bot import main, call_chain
-
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+import streamlit as st
+from bot import main, call_chain
 
 st.title("Simple Chat")
 
